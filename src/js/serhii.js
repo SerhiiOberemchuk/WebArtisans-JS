fe4aa3ebc5df02c2a0e146e4963f55dc2ffd9d00
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-11-26 09:49:35 +0100

    perPage

--- src/js/serhii.js
+++ src/js/serhii.js
@@ -1,41 +1,131 @@
-const ollProducts = [
-  {
-    _id: '640c2dd963a319ea671e3814',
-    name: 'Almonds',
-    img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3814.png',
-    category: 'Pantry_Items',
-    price: 8.99,
-    size: '16 oz bag',
-    is10PercentOff: false,
-    popularity: 552,
-  },
-  {
-    _id: '640c2dd963a319ea671e385e',
-    name: 'Ancho Chillies',
-    img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e385e.png',
-    category: 'Pantry_Items',
-    price: 4.99,
-    size: '100g',
-    is10PercentOff: false,
-    popularity: 632,
-  },
-];
-function writeLocalStorage() {
-  localStorage.setItem('ollProducts', JSON.stringify(ollProducts));
+// const ollProducts = [
+//   {
+//     _id: '640c2dd963a319ea671e3814',
+//     name: 'Almonds',
+//     img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3814.png',
+//     category: 'Pantry_Items',
+//     price: 8.99,
+//     size: '16 oz bag',
+//     is10PercentOff: false,
+//     popularity: 552,
+//   },
+//   {
+//     _id: '640c2dd963a319ea671e385e',
+//     name: 'Ancho Chillies',
+//     img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e385e.png',
+//     category: 'Pantry_Items',
+//     price: 4.99,
+//     size: '100g',
+//     is10PercentOff: false,
+//     popularity: 632,
+//   },
+// ];
+// function writeLocalStorage() {
+//   localStorage.setItem('ollProducts', JSON.stringify(ollProducts));
+// }
+// writeLocalStorage();
+
+import axios from 'axios';
+
+let pageOfRender = 1;
+let limitPage = 9;
+let totalPages = 60;
+
+const pagesBtnLeft = document.querySelector('.pages-btn-left');
+const pagesBtnRight = document.querySelector('.pages-btn-right');
+const numberOfPage = document.querySelector('.pages-list');
+pagesBtnLeft.addEventListener('click', onClickLeft);
+pagesBtnRight.addEventListener('click', onClickRight);
+numberOfPage.addEventListener('click', onClickNumber);
+
+function onClickLeft(event) {
+  if (pageOfRender === 1) {
+    return;
+  }
+  pageOfRender--;
+  console.log(pageOfRender);
+  mainResponse();
 }
-writeLocalStorage();
+function onClickRight(event) {
+  if (pageOfRender >= totalPages) {
+    return;
+  }
+  pageOfRender++;
+  console.log(pageOfRender);
+  mainResponse();
+}
+function onClickNumber(event) {
+  pageOfRender = Number(event.target.textContent);
+  if (!pageOfRender) {
+    return;
+  }
+  console.log(pageOfRender);
+  mainResponse();
+}
+//----------------------------------------------- MAIN RESPONSE
+
+async function mainResponse() {
+  try {
+    const response = await axios.get(
+      `https://food-boutique.b.goit.study/api/products?page=${pageOfRender}&limit=${limitPage}`
+    );
+    // console.log(response);
+    renderMainCards(response.data.results);
+    // totalPages = response.data.totalPages;
+    renderNumberSlider(totalPages);
+  } catch (error) {
+    console.error(error);
+  }
+}
+mainResponse();
+console.log(totalPages);
+// --------------------------------------------------DISCOUNT
+async function responseDiscount() {
+  try {
+    const response = await axios.get(
+      'https://food-boutique.b.goit.study/api/products/discount'
+    );
+    // console.log(response);
+    function getRandomNumber() {
+      return Math.floor(Math.random() * 9);
+    }
+    const randomNumber = getRandomNumber();
+
+    const end = randomNumber + 2;
+
+    const array = response.data.slice(randomNumber, end);
+    renderDiscountProducts(array);
+  } catch (error) {
+    console.error(error);
+  }
+}
+responseDiscount();
+//---------------------------------------------POPULAR PRODUCTS
+async function responsePopular() {
+  try {
+    const response = await axios.get(
+      'https://food-boutique.b.goit.study/api/products/popular?limit=5'
+    );
+    console.log(response.data[0]._id);
+    renderPopularProducts(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+responsePopular();
 
 const listOfMainProducts = document.querySelector('.basic-list');
 
-const savedSettings = localStorage.getItem('ollProducts');
-const parsedSettings = JSON.parse(savedSettings);
-console.log(parsedSettings);
+// const savedSettings = localStorage.getItem('ollProducts');
+// const parsedSettings = JSON.parse(savedSettings);
+// console.log(parsedSettings);
 
+//----------------------------------------------RENDER MAIN CARDS-------------------------------------------------//
 function renderMainCards(datas) {
   const murcap = datas
     .map(
       item =>
-        `<li class="basic-item">
+        `<li class="basic-item" id="${item._id}">
             <div class="basic-image-wrapper">
               <img
                 src="${item.img}"
@@ -56,7 +146,7 @@ function renderMainCards(datas) {
             </div>
             <div class="basic-last-info">
               <span class="basic-price">$${item.price}</span>
-              <button class="basic-btn" type="button">
+              <button class="basic-btn" type="button" id="${item._id}">
                 <svg class="basic-btn-icon" width="18" height="18">
                   <use href="./images/icons.svg#icon-basket"></use>
                 </svg>
@@ -65,24 +155,25 @@ function renderMainCards(datas) {
           </li>`
     )
     .join('');
-  console.log(murcap);
+  // console.log(murcap);
 
   listOfMainProducts.innerHTML = murcap;
 }
-renderMainCards(parsedSettings);
+// renderMainCards(parsedSettings);
 
+//----------------------------------------------RENDER POPULAR LIST-------------------------------------------------//
 const popularList = document.querySelector('.popular-list');
 function renderPopularProducts(datas) {
   const murcap = datas
     .map(
-      item => ` <li class="popular-item">
+      item => ` <li class="popular-item" id="${item._id}">
             <div class="popular-image-wrapper">
               <img src="${item.img}" alt="${item.name}" />
             </div>
             <div class="popular-info-wrapper">
               <div class="popular-name-wrapper">
                 <h3 class="popular-name">${item.name}</h3>
-                <button class="popular-item-btn" type="button" aria-label="add to basket">
+                <button class="popular-item-btn" type="button" aria-label="add to basket" id="${item._id}">
                   <svg class="popular-item-btn-icon" width="12" height="12">
                     <use href="./images/icons.svg#icon-basket"></use>
                   </svg>
@@ -98,15 +189,16 @@ function renderPopularProducts(datas) {
           </li>`
     )
     .join('');
+
   popularList.innerHTML = murcap;
 }
-renderPopularProducts(parsedSettings);
-
+// renderPopularProducts(parsedSettings);
+//----------------------------------------------RENDER DISCOUNT CARDS-------------------------------------------------//
 const discountListProduct = document.querySelector('.discount-list');
 function renderDiscountProducts(datas) {
   const murcap = datas
     .map(
-      item => `<li class="discount-item">
+      item => `<li class="discount-item" id="${item._id}">
             <div class="discount-label">
               <svg width="14" height="14">
                 <use href="./images/icons.svg#icon-leaf"></use>
@@ -118,7 +210,7 @@ function renderDiscountProducts(datas) {
             <div class="discount-info-wrapper">
               <h3 class="discount-item-name">${item.name}</h3>
               <span class="discount-item-price">$${item.price}</span>
-              <button class="basic-btn" type="button" aria-label="icon-basket">
+              <button class="basic-btn" type="button" aria-label="icon-basket" id="${item._id}">
                 <svg class="basic-btn-icon" width="18" height="18">
                   <use href="./images/icons.svg#icon-basket"></use>
                 </svg>
@@ -129,4 +221,54 @@ function renderDiscountProducts(datas) {
     .join('');
   discountListProduct.innerHTML = murcap;
 }
-renderDiscountProducts(parsedSettings);
+// renderDiscountProducts(parsedSettings);
+
+//-----------------------------------------------------------PAGINATION----------------------------------------//
+// const numberOfPage = document.querySelector('.pages-list');
+function renderNumberSlider(numberpages) {
+  let pagNum = [];
+  for (let index = 1; index <= numberpages; index++) {
+    pagNum.push(index);
+  }
+  if (numberpages <= 5) {
+    const murcap = pagNum
+      .map(item => `<li class="pages-item">${item}</li>`)
+      .join('');
+    console.log(murcap);
+
+    numberOfPage.innerHTML = murcap;
+  } else if (numberpages > 5) {
+    if (pageOfRender <= 2 || pageOfRender > numberpages - 2) {
+      const numToDel = numberpages - 4;
+      pagNum.splice(2, numToDel, '<li class="pages-item-points">...</li>');
+      const murcap = pagNum
+        .map(item => `<li class="pages-item">${item}</li>`)
+        .join('');
+      console.log(murcap);
+      numberOfPage.innerHTML = murcap;
+    } else if (pageOfRender > 2) {
+      const numToDel = numberpages - 2;
+      // const murcapIncide = `${pageOfRender - 1},${pageOfRender},${
+      //   pageOfRender + 1
+      // },`;
+      pagNum.splice(
+        1,
+        numToDel,
+        '...',
+        pageOfRender - 1,
+        pageOfRender,
+        pageOfRender + 1,
+        '...'
+      );
+      const murcap = pagNum
+        .map(item => `<li class="pages-item">${item}</li>`)
+        .join('');
+      numberOfPage.innerHTML = murcap;
+    }
+  }
+  // console.log(pagNum);
+}
+// renderNumberSlider(totalPages);
+
+//----------------------------------------------EXPORT------------------------------------------------------
+// export { renderMainCards, renderPopularProducts, renderDiscountProducts };
