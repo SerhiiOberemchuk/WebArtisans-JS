dff9679442f8a366915a594e8cb0a2cfa1e85064
Varenik575 <invisibleredhead5@gmail.com>
2023-11-27 18:16:16 +0200

    made footer subscription form field required

--- src/js/alex.js
+++ src/js/alex.js
@@ -6,6 +6,7 @@ footerSubscriptionForm.addEventListener('submit', onFooterFormSubmit);
 function onFooterFormSubmit(event) {
   event.preventDefault();
   console.log(footerSubscriptionForm.elements[0].value);
+
   axios.post('https://food-boutique.b.goit.study/api/subscription', {
     email: footerSubscriptionForm.elements[0].value,
   });
