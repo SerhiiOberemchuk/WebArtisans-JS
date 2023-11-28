cc2c09da9fa8a1d5f269283ebd25a349132c456a
SerhiiOberemchuk <135756668+SerhiiOberemchuk@users.noreply.github.com>
2023-11-28 22:53:38 +0000

    Deploying to gh-pages from @ SerhiiOberemchuk/WebArtisans-JS@f5ebb7a3b15255bb349324e9a7a48dfe110c51f1 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,23 +1,39 @@
-import"./assets/styles-63c1e7ac.js";import{a as S}from"./assets/vendor-a61d8330.js";const h="/WebArtisans-JS/assets/icons-69d7e34a.svg",l=document.querySelector(".scrol-list"),p="BASKET",a=document.querySelector(".empty-yellow-cart"),i=document.querySelector(".scrollbar-container"),u=document.querySelector(".order-container"),d=document.querySelector(".quantity-in-cart"),m=document.querySelector(".quantity-in-cart-header"),f=document.querySelector(".cart-sum-span");let e=JSON.parse(localStorage.getItem(`${p}`));console.log(e);function y(t){if(!t||t.length===0){a.style.display="block",i.style.display="none",u.style.display="none";return}const n=e.reduce((o,s)=>o+s.price,0).toFixed(2);f.textContent=`$${n}`;const r=t.map(o=>`<li class="scroll-item">
-              <button class="scroll-top-button" type="button" aria-label="1" id="${o._id}">
+import"./assets/styles-63c1e7ac.js";import{a as S}from"./assets/vendor-a61d8330.js";const i="/WebArtisans-JS/assets/icons-69d7e34a.svg",c=document.querySelector(".scrol-list"),l="BASKET",u=document.querySelector(".empty-yellow-cart"),d=document.querySelector(".scrollbar-container"),m=document.querySelector(".order-container"),p=document.querySelector(".quantity-in-cart"),g=document.querySelector(".quantity-in-cart-header"),$=document.querySelector(".cart-sum-span");let n=JSON.parse(localStorage.getItem(`${l}`));console.log(n);function y(){const s=n.reduce((t,o)=>t+o.price*o.amount,0).toFixed(2);$.textContent=`$${s}`}function b(e){if(!e||e.length===0){u.style.display="block",d.style.display="none",m.style.display="none";return}y();const s=e.map(t=>`<li class="scroll-item" id="${t._id}">
+              <button class="scroll-top-button" type="button" aria-label="1" id="${t._id}">
                 <svg class="scroll-top-icon" width="18" height="18">
-                  <use href="${h}#icon-close"></use>
+                  <use href="${i}#icon-close"></use>
                 </svg>
               </button>
               <div class="image-container">
-                <img class="scroll-image" src="${o.img}" alt="${o.name}" width="64" height="64" />
+                <img class="scroll-image" src="${t.img}" alt="${t.name}" width="64" height="64" />
               </div>
               <div class="text-container">
-                <h3 class="scroll-item-name">${o.name}</h3>
+                <h3 class="scroll-item-name">${t.name}</h3>
                 <div class="item-charact">
                   <p class="scroll-item-category">
-                    Category:<span class="span-scroll-category">${o.category}</span>
+                    Category:<span class="span-scroll-category">${t.category}</span>
                   </p>
                   <p class="scroll-item-size">
-                    Size:<span class="span-scroll-size">${o.size}</span>
+                    Size:<span class="span-scroll-size">${t.size}</span>
                   </p>
                 </div>
-                <p class="scroll-item-price">$${o.price}</p>
+                
+                <div class="price-amount">
+                    <p class="scroll-item-price" id="${t._id}price">$${t.price}</p>
+                    <div class="amount-item">
+                      <button type="button" class="button-item-plus" id="${t._id}">
+                        <svg class="plus-icon">
+                          <use href="${i}#icon-plus"></use>
+                        </svg>
+                      </button>
+                      <span class="amount-number" id="${t._id}amount">${t.amount}</span>
+                      <button type="button" class="button-item-minus" id="${t._id}">
+                        <svg class="minus-icon">
+                          <use href="${i}#icon-minus"></use>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
               </div>
-            </li>`).join("");l.innerHTML=r,d.textContent=t.length,m.textContent=t.length}y(e);const C=document.querySelector(".cart-button");C.addEventListener("click",b);function b(t){a.style.display="block",i.style.display="none",u.style.display="none",d.textContent=0,m.textContent=0,localStorage.removeItem("BASKET"),l.innerHTML=""}l.addEventListener("click",v);function v(t){if(!t.target.classList.contains("scroll-top-button"))return;const c=t.target.id,n=e.findIndex(r=>r._id===c);n!==-1&&(e.splice(n,1),console.log(e),y(e),localStorage.setItem(`${p}`,JSON.stringify(e))),e.length||(d.textContent=e.length,m.textContent=e.length,a.style.display="block",i.style.display="none",u.style.display="none")}const g="BASKET";function $(){return JSON.parse(localStorage.getItem(`${g}`))||[]}async function q(){try{const t=document.querySelector("#user-email");if(!t||!t.value){alert("Write your Email.");return}const c=t.value,n=$();if(!n||n.length===0){alert("Your cart is empty. Add some items before checking out.");return}const r={email:c,products:n.map(s=>({productId:s._id,amount:s.quantity}))},o=await S.post("https://food-boutique.b.goit.study/api/orders",r);alert("Order created successfully!"),localStorage.removeItem(`${g}`)}catch(t){console.error("Error creating order:",t),alert(t.response.data.message||"Error creating order. Please try again.")}}document.getElementById("checkoutBtn").addEventListener("click",q);
+            </li>`).join("");c.innerHTML=s,p.textContent=e.length,g.textContent=e.length}b(n);const h=document.querySelector(".cart-button");h.addEventListener("click",v);function v(e){u.style.display="block",d.style.display="none",m.style.display="none",p.textContent=0,g.textContent=0,localStorage.removeItem("BASKET"),c.innerHTML=""}c.addEventListener("click",C);function C(e){if(!e.target.classList.contains("scroll-top-button"))return;const s=e.target.id,t=n.findIndex(o=>o._id===s);t!==-1&&(n.splice(t,1),console.log(n),b(n),localStorage.setItem(`${l}`,JSON.stringify(n))),n.length||(p.textContent=n.length,g.textContent=n.length,u.style.display="block",d.style.display="none",m.style.display="none")}document.querySelector(".amount-number");c.addEventListener("click",I);function I(e){if(!e.target.classList.contains("button-item-plus"))return;const s=e.target.id,t=n.findIndex(r=>r._id===s);let o=Number(n[t].amount);function a(){o++,n[t].amount=o,y()}a(),localStorage.setItem(`${l}`,JSON.stringify(n)),document.getElementById(`${s}amount`).textContent=o}c.addEventListener("click",x);function x(e){const s=e.target.id,t=n.findIndex(r=>r._id===s);let o=Number(n[t].amount);if(!e.target.classList.contains("button-item-minus")||o===1)return;function a(){o--,n[t].amount=o,y()}a(),localStorage.setItem(`${l}`,JSON.stringify(n)),document.getElementById(`${s}amount`).textContent=o}const f="BASKET";function E(){return JSON.parse(localStorage.getItem(`${f}`))||[]}async function q(){try{const e=document.querySelector("#user-email");if(!e||!e.value){alert("Write your Email.");return}const s=e.value,t=E();if(!t||t.length===0){alert("Your cart is empty. Add some items before checking out.");return}const o={email:s,products:t.map(r=>({productId:r._id,amount:r.quantity}))},a=await S.post("https://food-boutique.b.goit.study/api/orders",o);alert("Order created successfully!"),localStorage.removeItem(`${f}`)}catch(e){console.error("Error creating order:",e),alert(e.response.data.message||"Error creating order. Please try again.")}}document.getElementById("checkoutBtn").addEventListener("click",q);
 //# sourceMappingURL=commonHelpers.js.map
